109c1fcabdbc17a471776039639e3c05d3e6ca0f
Samuel Stauffer <samuel@descolada.com>
2013-06-26 14:44:51 -0700

    Use int32 for nodes to save memory

--- astar.go
+++ astar.go
@@ -24,41 +24,39 @@ type Graph interface {
 }
 
 type nodeInfo struct {
-	node          int
 	parent        *nodeInfo // the node from which we came to get here
-	count         int       // count lets use know how long the path is when we reach the end without traversing it
-	cost          float64   // current cost from start node to this node
-	predictedCost float64   // heuristic cost from this node to end node
-	index         int       // index of the node in the heap
-	open          bool
+	node          int32
+	index         int32   // index of the node in the heap
+	cost          float32 // current cost from start node to this node
+	predictedCost float32 // heuristic cost from this node to end node
 }
 
 type state struct {
-	info map[int]*nodeInfo
+	info map[int32]*nodeInfo
 	heap []*nodeInfo
 }
 
 func newState(capacity int) *state {
 	return &state{
-		info: make(map[int]*nodeInfo, capacity),
+		info: make(map[int32]*nodeInfo, capacity),
 		heap: make([]*nodeInfo, 0, defaultListCapacity),
 	}
 }
 
-func (nl *state) less(i, j int) bool {
+func (nl *state) less(i, j int32) bool {
 	li := nl.heap[i]
 	lj := nl.heap[j]
 	return (li.cost + li.predictedCost) < (lj.cost + lj.predictedCost)
 }
 
-func (nl *state) swap(i, j int) {
+func (nl *state) swap(i, j int32) {
 	l := nl.heap
 	l[i], l[j] = l[j], l[i]
 	l[i].index = i
 	l[j].index = j
 }
 
-func (nl *state) up(j int) {
+func (nl *state) up(j int32) {
 	for {
 		i := (j - 1) / 2 // parent
 		if i == j || !nl.less(j, i) {
@@ -69,7 +67,7 @@ func (nl *state) up(j int) {
 	}
 }
 
-func (nl *state) down(i, n int) {
+func (nl *state) down(i, n int32) {
 	for {
 		j1 := 2*i + 1
 		if j1 >= n || j1 < 0 { // j1 < 0 after int overflow
@@ -88,7 +86,7 @@ func (nl *state) down(i, n int) {
 }
 
 func (nl *state) popBest() *nodeInfo {
-	n := len(nl.heap) - 1
+	n := int32(len(nl.heap) - 1)
 	if n < 0 {
 		return nil
 	}
@@ -103,13 +101,14 @@ func (nl *state) popBest() *nodeInfo {
 func (nl *state) addNodeInfo(ni *nodeInfo) {
 	nl.info[ni.node] = ni
 	nl.heap = append(nl.heap, ni)
-	ni.index = len(nl.heap) - 1
-	nl.up(len(nl.heap) - 1)
+	i := int32(len(nl.heap) - 1)
+	ni.index = i
+	nl.up(i)
 }
 
 func (nl *state) updateNodeInfo(ni *nodeInfo) {
 	index := ni.index
-	n := len(nl.heap)
+	n := int32(len(nl.heap))
 	nl.down(index, n)
 	nl.up(index)
 }
@@ -133,12 +132,10 @@ func FindPath(mp Graph, start, end int) ([]int, error) {
 		return nil, err
 	}
 	state.addNodeInfo(&nodeInfo{
-		node:          start,
+		node:          int32(start),
 		parent:        nil,
-		count:         1,
-		cost:          0,
-		predictedCost: pCost,
-		open:          true,
+		cost:          0.0,
+		predictedCost: float32(pCost),
 	})
 
 	edgeSlice := make([]Edge, 0, 8)
@@ -147,54 +144,54 @@ func FindPath(mp Graph, start, end int) ([]int, error) {
 		if current == nil {
 			return nil, ErrImpossible
 		}
-		if current.node == end {
+		if current.node == int32(end) {
 			// If we reached the end node then we know the optimal path. Traverse
 			// it (backwards) and return an array of node IDs.
-			path := make([]int, current.count)
-			for i, n := current.count-1, current; n != nil; i, n = i-1, n.parent {
-				path[i] = n.node
+			path := make([]int, 0, 128)
+			for n := current; n != nil; n = n.parent {
+				path = append(path, int(n.node))
+			}
+			// Reverse the path since we built it backwards
+			n := len(path) / 2
+			for i := 0; i < n; i++ {
+				j := len(path) - i - 1
+				path[i], path[j] = path[j], path[i]
 			}
 			return path, nil
 		}
-		current.open = false
-		neighbors, err := mp.Neighbors(current.node, edgeSlice[:0])
+		neighbors, err := mp.Neighbors(int(current.node), edgeSlice[:0])
 		if err != nil {
 			return nil, err
 		}
 		for _, edge := range neighbors {
 			// Don't try go backwards
-			if current.parent != nil && edge.Node == current.parent.node {
+			if current.parent != nil && int32(edge.Node) == current.parent.node {
 				continue
 			}
 
 			// Cost for the neighbor node is the current cost plus the
 			// cost to get to that node.
-			cost := current.cost + edge.Cost
+			cost := current.cost + float32(edge.Cost)
 
-			if ni := state.info[edge.Node]; ni == nil {
+			if ni := state.info[int32(edge.Node)]; ni == nil {
 				// We haven't seen this node so add it to the open list.
 				pCost, err := mp.HeuristicCost(edge.Node, end)
 				if err != nil {
 					return nil, err
 				}
 				state.addNodeInfo(&nodeInfo{
-					node:          edge.Node,
+					node:          int32(edge.Node),
 					parent:        current,
-					count:         current.count + 1,
 					cost:          cost,
-					predictedCost: pCost,
-					open:          true,
+					predictedCost: float32(pCost),
 				})
 			} else if cost < ni.cost {
 				// We've seen this node and the current path is cheaper
 				// so update the changed info and add it to the open list
 				// (replacing if necessary).
-				wasOpen := ni.open
-				ni.open = true
 				ni.parent = current
-				ni.count = current.count + 1
 				ni.cost = cost
-				if wasOpen {
+				if ni.index >= 0 {
 					state.updateNodeInfo(ni)
 				} else {
 					state.addNodeInfo(ni)
